f482622e78adf60b303735276650e73bb0888a0f
mohammad khamis <m@khamis.info>
2025-04-30 21:31:01 +0300

    fairy v0

--- src/utils/models.js
+++ src/utils/models.js
@@ -17,6 +17,31 @@ export const models_data = {
       "modelFolder": "cleaningsupplies",
       "modelName": "cleaningsupplies",
       "labels": ['Bleache', 'Dishwashing Detergent', 'Kilogram Soap', 'Liquid Detergent', 'Matic Detergent', 'Paper towel', 'Pressed Soap', 'Softener', 'Wet Wipes'],
+    },
+    {
+      title: "Fairy V0",
+      modelFolder: "fairyv0",
+      modelName: "fairyv0",
+      labels: [
+        "ADW AIO 4X71CT",
+        "ADW AIO 5X41CT",
+        "ADW AIO YELLOW 4X48CT CEE GG",
+        "ADW PLAT 4X66CT",
+        "ADW PLAT 5X39CT",
+        "ADW PLAT YELLOW 4X37CT CON EU RV2",
+        "HDW AVERA&CUCUM 16X515ML IL",
+        "HDW LMN 8X650ML MAXPWR",
+        "HDW Lemon 9X1190ML",
+        "HDW ORG 8X650ML MAXPWR",
+        "HDW Original 9X1190ML",
+        "HDW PLAT LMN 16X515ML IL",
+        "HDW PLAT ORG 16X515ML IL",
+        "HDW ROSE&SATIN 16X515ML IL",
+        "HDW Ultra LMN 8X900ML",
+        "HDW Ultra ORG 8X900ML",
+        "Lemon 16X650 ML",
+        "ORIGINAL BO 16X650ML IL"
+      ]
     }
   ]
 }
